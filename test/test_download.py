9665ddddec058afa7cd27eeebc6afe4799659dc8
senichka123 <73533496+senichka123@users.noreply.github.com>
2020-11-16 16:34:04 +0300

    Add files via upload

--- test/test_download.py
+++ test/test_download.py
@@ -6,12 +6,14 @@ from bin.download import gaiadata
 
 class NumberOfClustersTestCase(unittest.TestCase):
 	def test_datasize(self):
-		actual, b = gaiadata(124.90, -38.27)
+		actual= gaiadata(124.90, -38.27)
 		desired = pd.read_csv('data.csv')
 		assert actual.size == desired.size
 
 	def test_columnname(self):
-		a, actual = gaiadata(124.90, -38.27)
+		Data = gaiadata(124.90, -38.27)
+		a = list(Data)
+		actual = b = [a[0],a[1],a[4],a[6],a[8]]
 		desired = ['RA_ICRS', 'DE_ICRS', 'Plx', 'pmRA', 'pmDE']
 		assert actual == desired
 if __name__ == '__main__':
