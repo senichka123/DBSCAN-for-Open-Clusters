5bbf3a9262dbf5f563aacd55b0c38e011db92d44
senichka123 <73533496+senichka123@users.noreply.github.com>
2020-12-16 19:03:15 +0300

    Update show.py

--- ocscan/show.py
+++ ocscan/show.py
@@ -1,6 +1,7 @@
 import numpy as np
 
-def NumberOfClusters(labels):    
+def NumberOfClusters(labels): 	
+    """Show results of clustering for DBSCAN"""
     n_clusters_ = len(set(labels)) - (1 if -1 in labels else 0)
     print('Estimated number of clusters: %d' % (n_clusters_))
     return n_clusters_
