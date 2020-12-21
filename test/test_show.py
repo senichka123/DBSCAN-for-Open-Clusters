c0c89983709ca979fc007eef918b92346a6d5dbf
senichka123 <73533496+senichka123@users.noreply.github.com>
2020-12-21 12:12:58 +0300

    Update test_show.py

--- test/test_show.py
+++ test/test_show.py
@@ -4,12 +4,14 @@ import pandas as pd
 import sys
 
 from ocscan.dbscan import dbscan
+from ocscan.download import gaiadata
 from ocscan.show import NumberOfClusters
 
 class NumberOfClustersTestCase(unittest.TestCase):
 	def test_result(self):
-		labels = dbscan(22, 0.3, 124.90, -38.27, plot=False, save=False)
-		actual = NumberOfClusters(labels)
+		data = gaiadata(124.90, -38.27)
+		mytuple = dbscan(data, 22, 0.3)
+		actual = NumberOfClusters(mytuple[1])
 		desired = 1
 		assert actual == desired, f'Estimated number of clusters: {actual} != {desired}'
 
