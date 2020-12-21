913dbaeec3b8ff5f9c867fd056aff04928065a48
senichka123 <73533496+senichka123@users.noreply.github.com>
2020-12-21 12:10:33 +0300

    Update save.py

--- ocscan/save.py
+++ ocscan/save.py
@@ -1,14 +1,8 @@
 import numpy as np
 
-def results(Data, labels, core_samples_mask):   
+def results(Data, labels, core_samples_mask):    
     """Save results into .csv file"""
-    unique_labels = set(labels)
-    for k in unique_labels:
-        if k == -1:
-            continue
-        class_member_mask = (labels == k)
-        
-    Data[class_member_mask & core_samples_mask].to_csv('res-centers.csv',index=False, mode = 'w')
-    Data[class_member_mask & ~core_samples_mask].to_csv('res-neighbours.csv',index=False, mode = 'w')
-    Data[class_member_mask].to_csv('res-cluster.csv',index=False, mode = 'w')
+    Data[(labels == 0) & core_samples_mask].to_csv('res-centers.csv',index=False, mode = 'w')
+    Data[(labels == 0) & ~core_samples_mask].to_csv('res-neighbours.csv',index=False, mode = 'w')
+    Data[(labels == 0)].to_csv('res-cluster.csv',index=False, mode = 'w')
 
