0829b3bdeb0d707ebde5512e0cb87803b80c2b03
senichka123 <73533496+senichka123@users.noreply.github.com>
2020-12-21 12:07:16 +0300

    Update dbscan.py

--- ocscan/dbscan.py
+++ ocscan/dbscan.py
@@ -12,29 +12,21 @@ import ocscan.show
 
 
 	
-def dbscan(N, eps, ra, de, plot=False, save=False, input=False):
+def dbscan(data, N, eps, input=False):
 	"""Searching for clusters in data on the basis of DBSCAN algorithm and Principal Component Analysis in 4D hyper-sphere"""
-	data = ocscan.download.gaiadata(ra, de, SaveData = input) # ra,dec,radius in degrees (radius_default = 0.0833 -> 5 arcmin)
-									  # download data from gaia dr2
 	names=['pmRA','pmDE','RA_ICRS','DE_ICRS'] 
 	N_dim = len(names)				  # dimensions. 					  
 									  # physical 4D-space
-	mask=np.array([True for i in range(len(data))])
+	mask = np.array([True for i in range(len(data))])
 	for x in names:
-		mask=mask & data[x].notna()   # deleting empty data lines (table strings)
+		mask = mask & data[x].notna()   # deleting empty data lines (table strings)
 
-	Data=data[mask]   
+	Data = data[mask]   
 	NormMainData = StandardScaler().fit_transform(Data[names])
-	#NormMainData = np.true_divide(Data[names], list(Data[names].std()))
 	db = DBSCAN(eps, N).fit(NormMainData)
-	Data['labels']=db.labels_
+	Data['labels'] = db.labels_
 	core_samples_mask = np.zeros_like(db.labels_, dtype=bool)
 	core_samples_mask[db.core_sample_indices_] = True
 	
-	if plot:
-		ocscan.plot.graphs(Data, db.labels_, core_samples_mask)
-	if save:
-		ocscan.save.results(Data, db.labels_, core_samples_mask)
-		
-	return db.labels_
+	return Data, db.labels_, core_samples_mask
 	
