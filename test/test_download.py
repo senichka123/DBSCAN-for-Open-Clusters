2842da3cc49e07ccab055758f7e1bf6f44fb1b0e
senichka123 <73533496+senichka123@users.noreply.github.com>
2020-12-21 12:12:31 +0300

    Update test_download.py

--- test/test_download.py
+++ test/test_download.py
@@ -1,19 +1,15 @@
 import doctest
 import unittest
 import pandas as pd
+import tempfile
 
 from ocscan.download import gaiadata
 
 class NumberOfClustersTestCase(unittest.TestCase):
-	def test_datasize(self):
-		actual= gaiadata(124.90, -38.27, SaveData = True)
-		desired = pd.read_csv('data.csv')
-		assert actual.size == desired.size
-
 	def test_columnname(self):
-		Data = gaiadata(124.90, -38.27, SaveData = True)
+		Data = gaiadata(124.90, -38.27, save_data = True)
 		a = list(Data)
-		actual = b = [a[0],a[1],a[4],a[6],a[8]]
+		actual = [a[0],a[1],a[4],a[6],a[8]]
 		desired = ['RA_ICRS', 'DE_ICRS', 'Plx', 'pmRA', 'pmDE']
 		assert actual == desired
 if __name__ == '__main__':
